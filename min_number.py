c3dc741aeb625695fd1ef74bdc80c536e93a6697
Ubuntu <ubuntu@ip-172-31-11-30.ec2.internal>
2021-01-13 17:08:34 +0000

    added min_number.py

---
+++ min_number.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+# Created by Marlon Poddalgoda
+# Created on January 2021
+# This program prints 10 random integers and finds the smallest
+
+import random
+
+
+def min_value(random_list):
+    # This function finds the smallest number
+
+    min_num = random_list[0]
+
+    for loop_counter in random_list:
+        if min_num <= loop_counter:
+            continue
+        elif min_num > loop_counter:
+            min_num = loop_counter
+
+    return min_num
+
+
+def main():
+    # This function prints 10 random integers and output
+
+    print("This program prints 10 random integers and"
+          " finds the smallest number.")
+    print("")
+
+    random_list = []
+
+    print("Here are 10 random numbers:")
+
+    # process
+    for loop_counter in range(0, 10):
+        # random number
+        random_number = random.randint(1, 100)
+        random_list.append(random_number)
+        print(random_number)
+
+    # call function
+    smallest_num = min_value(random_list)
+
+    print("")
+    print("The smallest number is: {0}".format(smallest_num))
+
+
+if __name__ == "__main__":
+    main()
